fb1222a619120232b09b908e0c77cf3c16b3727c
Tugrul Ates <me@tugrulates.com>
2025-02-27 17:32:21 +0100

    describe tests

--- tool/forge/app.test.ts
+++ tool/forge/app.test.ts
@@ -1,15 +1,15 @@
 import { version } from "@roka/forge/app";
 import { assertMatch } from "@std/assert";
 
-Deno.test("version()", async () => {
+Deno.test("version() provides version", async () => {
   assertMatch(await version(), /[^\s]+/);
 });
 
-Deno.test("version({ build: true })", async () => {
+Deno.test("version({ build: true }) includes build target", async () => {
   assertMatch(await version({ build: true }), /[^\s]+ \([^\s]+\)/);
 });
 
-Deno.test("version({ deno: true })", async () => {
+Deno.test("version({ deno: true }) include deno versions as lines", async () => {
   assertMatch(
     await version({ deno: true }),
     /[^\s]+\ndeno [^\s]+\nv8 [^\s]+\ntypescript [^\s]+/,
