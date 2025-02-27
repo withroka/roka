3b8b9b423d9a4dd5702e48b7133ea71712ac034b
Tugrul Ates <me@tugrulates.com>
2025-02-27 17:34:20 +0100

    feat(forge): options for `version` (#67)

--- tool/forge/app.ts
+++ tool/forge/app.ts
@@ -18,10 +18,36 @@ import { PackageError, packageInfo } from "@roka/forge/package";
 import { expandGlob } from "@std/fs";
 import { dirname, join } from "@std/path";
 
+/** Options for {@linkcode version}. */
+export interface VersionOptions {
+  /**
+   * Add build information to the version.
+   * @default {false}
+   */
+  build?: boolean;
+  /**
+   * Add Deno version, including v8 and typescript, to the version.
+   * @default {false}
+   */
+  deno?: boolean;
+}
+
 /**
  * Returns the version of the current package.
  *
  * Useful for providing a version number to the user of a tool or application.
+ *
+ * @example
+ * ```ts
+ * await version();                // => 1.0.0
+ * await version({ build: true }); // => 1.0.0 (aarch64-apple-darwin)
+ * await version({ deno: true });  // => 1.0.0
+ *                                 //    deno 2.2.2
+ *                                 //    v8 13.4.114.9-rusty
+ *                                 //    typescript 5.7.3
+ * ```
+ *
+ * @example Version string with Deno version information.
  *
  * The version is determined from whichever is available first:
  *  - release tags and {@link https://www.conventionalcommits.org | Conventional Commits} (local development)
@@ -29,12 +55,20 @@ import { dirname, join } from "@std/path";
  *  - config version from `deno.json` in the dist directory (deno compile)
  *  - "(unknown)" if none of the above are available
  */
-export async function version(): Promise<string> {
+export async function version(options?: VersionOptions): Promise<string> {
   return [
-    `${await packageVersion()} (${Deno.build.target})`,
-    `deno ${Deno.version.deno}`,
-    `v8 ${Deno.version.v8}`,
-    `typescript ${Deno.version.typescript}`,
+    [
+      await packageVersion(),
+      ...options?.build ? [`(${Deno.build.target})`] : [],
+    ]
+      .join(" "),
+    ...options?.deno
+      ? [
+        `deno ${Deno.version.deno}`,
+        `v8 ${Deno.version.v8}`,
+        `typescript ${Deno.version.typescript}`,
+      ]
+      : [],
   ].join("\n");
 }
 
