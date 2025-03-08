72e6bd08562b7553a6386bc3c7ddbca3b8ebec64
Tugrul Ates <me@tugrulates.com>
2025-03-08 13:34:01 +0100

    faet(forge): add examples to CLI help

--- tool/forge/forge.ts
+++ tool/forge/forge.ts
@@ -86,84 +86,12 @@ import { release } from "@roka/forge/release";
 import { version } from "@roka/forge/version";
 import { join, relative } from "@std/path";
 
-function compileCommand(targets: string[]) {
-  return new Command()
-    .description("Compile packages into binary executables.")
-    .arguments("[packages...:file]")
-    .type("target", new EnumType(targets))
-    .option("--target=<architecture:target>", "Target OS architecture.", {
-      collect: true,
-    })
-    .option("--release", "Use new release version.", { default: false })
-    .option("--bundle", "Zip and bundle artfifacts.", { default: false })
-    .option("--checksum", "Create a checksum file.", { default: false })
-    .option("--install=[directory:file]", "Install for local user.")
-    .option("--concurrency=<number:number>", "Max concurrent compilations.")
-    .action(async (options, ...filters) => {
-      const packages = (await workspace({ filters }))
-        .filter((pkg) => pkg.config.compile);
-      await pool(
-        packages,
-        async (pkg) => {
-          const artifacts = await compile(pkg, options);
-          console.log(`📦 Compiled ${pkg.name}`);
-          artifacts.forEach((artifact) => console.log("🏺", artifact));
-          if (options.install) console.log(`🧩 Installed ${pkg.name}`);
-        },
-        options,
-      );
-    });
-}
-
-function bumpCommand() {
-  return new Command()
-    .description("Bump versions on package config files.")
-    .arguments("[packages...:file]")
-    .option("--pr", "Create a pull request.", { default: false })
-    .env("GIT_NAME=<name:string>", "Git user name for the bump commit.", {
-      prefix: "GIT_",
-    })
-    .env("GIT_EMAIL=<email:string>", "Git user e-mail for the bump commit.", {
-      prefix: "GIT_",
-    })
-    .env(
-      "GITHUB_TOKEN=<token:string>",
-      "GitHub personal token for GitHub actions.",
-      { prefix: "GITHUB_" },
-    )
-    .action(async (options, ...filters) => {
-      const packages = (await workspace({ filters }))
-        .filter((pkg) => pkg.update);
-      const pr = await bump(packages, options);
-      if (pr) console.log(`🚀 Created version bump pull request [${pr.url}]`);
-      else console.log("📦 Bumped package versions");
-    });
-}
-
-function releaseCommand() {
-  return new Command()
-    .description("Creates releases for updated packages.")
-    .option("--draft", "Create a draft release.", { default: false })
-    .arguments("[packages...:file]")
-    .env(
-      "GITHUB_TOKEN=<token:string>",
-      "GitHub personal token for GitHub actions.",
-      { prefix: "GITHUB_", required: true },
-    )
-    .action(async (options, ...filters) => {
-      const packages = (await workspace({ filters }))
-        .filter((pkg) => pkg.config.version !== pkg.release?.version);
-      await pool(packages, async (pkg) => {
-        const [rls, assets] = await release(pkg, options);
-        console.log(`🚀 Released ${pkg.name} [${rls.url}]`);
-        assets.forEach((x) => console.log(`🏺 ${x.name} [${x.url}]`));
-      }, { concurrency: 1 });
-    });
-}
-
 function listCommand() {
   return new Command()
     .description("List packages, versions, and changelogs.")
+    .example("forge list", "List all packages.")
+    .example("forge list --modules", "List all modules.")
+    .example("forge list --changelog", "Display package changelogs.")
     .arguments("[packages...:file]")
     .option("--modules", "Print exported package modules.", { default: false })
     .option("--changelog", "Print package changelog.", { default: false })
@@ -194,10 +122,100 @@ function modulesText(pkg: Package): string | undefined {
     .join("\n");
 }
 
+function compileCommand(targets: string[]) {
+  return new Command()
+    .description("Compile packages into binary executables.")
+    .example("forge compile", "Compile packages.")
+    .example("forge compile --install", "Install binaries.")
+    .example(
+      "forge compile --release --bundle --checksum",
+      "Create release assets.",
+    )
+    .arguments("[packages...:file]")
+    .type("target", new EnumType(targets))
+    .option("--target=<architecture:target>", "Target OS architecture.", {
+      collect: true,
+    })
+    .option("--release", "Use new release version.", { default: false })
+    .option("--bundle", "Zip and bundle artfifacts.", { default: false })
+    .option("--checksum", "Create a checksum file.", { default: false })
+    .option("--install=[directory:file]", "Install for local user.")
+    .option("--concurrency=<number:number>", "Max concurrent compilations.")
+    .action(async (options, ...filters) => {
+      const packages = (await workspace({ filters }))
+        .filter((pkg) => pkg.config.compile);
+      await pool(
+        packages,
+        async (pkg) => {
+          const artifacts = await compile(pkg, options);
+          console.log(`📦 Compiled ${pkg.name}`);
+          artifacts.forEach((artifact) => console.log("🏺", artifact));
+          if (options.install) console.log(`🧩 Installed ${pkg.name}`);
+        },
+        options,
+      );
+    });
+}
+
+function bumpCommand() {
+  return new Command()
+    .description("Bump versions on package config files.")
+    .example("forge bump", "Bump versions.")
+    .example("forge bump --pr", "Create a version bump PR.")
+    .arguments("[packages...:file]")
+    .option("--pr", "Create a pull request.", { default: false })
+    .env("GIT_NAME=<name:string>", "Git user name for the bump commit.", {
+      prefix: "GIT_",
+    })
+    .env("GIT_EMAIL=<email:string>", "Git user e-mail for the bump commit.", {
+      prefix: "GIT_",
+    })
+    .env(
+      "GITHUB_TOKEN=<token:string>",
+      "GitHub personal token for GitHub actions.",
+      { prefix: "GITHUB_" },
+    )
+    .action(async (options, ...filters) => {
+      const packages = (await workspace({ filters }))
+        .filter((pkg) => pkg.update);
+      const pr = await bump(packages, options);
+      if (pr) console.log(`🚀 Created version bump pull request [${pr.url}]`);
+      else console.log("📦 Bumped package versions");
+    });
+}
+
+function releaseCommand() {
+  return new Command()
+    .description("Creates releases for updated packages.")
+    .example("forge release", "Create releases and tags for all updates.")
+    .example("forge release --draft", "Create draft releases for all updates.")
+    .option("--draft", "Create a draft release.", { default: false })
+    .arguments("[packages...:file]")
+    .env(
+      "GITHUB_TOKEN=<token:string>",
+      "GitHub personal token for GitHub actions.",
+      { prefix: "GITHUB_", required: true },
+    )
+    .action(async (options, ...filters) => {
+      const packages = (await workspace({ filters }))
+        .filter((pkg) => pkg.config.version !== pkg.release?.version);
+      await pool(packages, async (pkg) => {
+        const [rls, assets] = await release(pkg, options);
+        console.log(`🚀 Released ${pkg.name} [${rls.url}]`);
+        assets.forEach((x) => console.log(`🏺 ${x.name} [${x.url}]`));
+      }, { concurrency: 1 });
+    });
+}
+
 if (import.meta.main) {
   await new Command()
     .name("forge")
     .description("Manage packages.")
+    .example("forge", "List all packages.")
+    .example("forge list 'core/*'", "List packages in the 'core' directory.")
+    .example("forge compile --install", "Compile and install all binaries.")
+    .example("forge bump --pr", "Bump versions and create a bump PR.")
+    .example("forge release --draft", "Create releases with compiled assets.")
     .usage("<command> [options] [packages...]")
     .version(await version({ build: true, deno: true }))
     .default("list")
