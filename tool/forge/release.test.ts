b10c2a4f6e83b3e5b4f742d87b39e1653c60a78c
Tugrul Ates <me@tugrulates.com>
2025-03-12 12:02:59 +0100

    feat(forge): `testing` objects can initialize their git repositories

--- tool/forge/release.test.ts
+++ tool/forge/release.test.ts
@@ -1,67 +1,52 @@
-import { PackageError, packageInfo } from "@roka/forge/package";
+import { PackageError } from "@roka/forge/package";
 import { release } from "@roka/forge/release";
-import { testPackage } from "@roka/forge/testing";
-import { tempRepository } from "@roka/git/testing";
+import { tempPackage } from "@roka/forge/testing";
 import { fakeRelease, fakeRepository } from "@roka/github/testing";
 import { assertEquals, assertRejects } from "@std/assert";
+import { git } from "../../core/git/git.ts";
 
 Deno.test("release() rejects package without version", async () => {
-  await using git = await tempRepository();
-  const repo = fakeRepository({ git });
-  await git.commits.create("initial", { allowEmpty: true });
-  await testPackage(git.path(), { name: "@scope/name" });
-  await git.index.add("deno.json");
-  await git.commits.create("feat: new feature", { allowEmpty: true });
-  const pkg = await packageInfo({ directory: git.path() });
+  await using pkg = await tempPackage({
+    config: { name: "@scope/name" },
+    commits: [{ summary: "feat: new feature" }],
+  });
+  const repo = fakeRepository({ git: git({ cwd: pkg.directory }) });
   await assertRejects(() => release(pkg, { repo }), PackageError);
 });
 
 Deno.test("release() rejects version downgrade", async () => {
-  await using git = await tempRepository();
-  const repo = fakeRepository({ git });
-  await git.commits.create("initial", { allowEmpty: true });
-  await testPackage(git.path(), { name: "@scope/name", version: "0.1.0" });
-  await git.index.add("deno.json");
-  await git.commits.create("feat: new feature", { allowEmpty: true });
-  await git.tags.create("name@0.1.1");
-  const pkg = await packageInfo({ directory: git.path() });
+  await using pkg = await tempPackage({
+    config: { name: "@scope/name", version: "0.1.0" },
+    commits: [{ summary: "feat: new feature", tags: ["name@0.1.1"] }],
+  });
+  const repo = fakeRepository({ git: git({ cwd: pkg.directory }) });
   await assertRejects(() => release(pkg, { repo }), PackageError);
 });
 
 Deno.test("release() rejects no change", async () => {
-  await using git = await tempRepository();
-  const repo = fakeRepository({ git });
-  await git.commits.create("initial", { allowEmpty: true });
-  await testPackage(git.path(), { name: "@scope/name", version: "0.1.0" });
-  await git.index.add("deno.json");
-  await git.commits.create("feat: new feature", { allowEmpty: true });
-  await git.tags.create("name@0.1.0");
-  const pkg = await packageInfo({ directory: git.path() });
+  await using pkg = await tempPackage({
+    config: { name: "@scope/name", version: "0.1.0" },
+    commits: [{ summary: "feat: new feature", tags: ["name@0.1.0"] }],
+  });
+  const repo = fakeRepository({ git: git({ cwd: pkg.directory }) });
   await assertRejects(() => release(pkg, { repo }), PackageError);
 });
 
 Deno.test("release() rejects 0.0.0", async () => {
-  await using git = await tempRepository();
-  const repo = fakeRepository({ git });
-  await git.commits.create("initial", { allowEmpty: true });
-  await testPackage(git.path(), { name: "@scope/name", version: "0.0.0" });
-  await git.index.add("deno.json");
-  await git.commits.create("feat: new feature", { allowEmpty: true });
-  const pkg = await packageInfo({ directory: git.path() });
+  await using pkg = await tempPackage({
+    config: { name: "@scope/name", version: "0.0.0" },
+    commits: [{ summary: "feat: new feature" }],
+  });
+  const repo = fakeRepository({ git: git({ cwd: pkg.directory }) });
   await assertRejects(() => release(pkg, { repo }), PackageError);
 });
 
 Deno.test("release() creates initial release", async () => {
-  await using git = await tempRepository();
-  const repo = fakeRepository({ git });
-  await git.commits.create("initial", { allowEmpty: true });
-  await testPackage(git.path(), {
-    name: "@scope/name",
-    version: "0.1.0",
+  await using pkg = await tempPackage({
+    config: { name: "@scope/name", version: "0.1.0" },
+    commits: [{ summary: "feat: new feature" }],
   });
-  await git.index.add("deno.json");
-  await git.commits.create("feat: new feature", { allowEmpty: true });
-  const pkg = await packageInfo({ directory: git.path() });
+  const repo = fakeRepository({ git: git({ cwd: pkg.directory }) });
   const [rls, assets] = await release(pkg, { repo });
   assertEquals(rls.tag, "name@0.1.0");
   assertEquals(rls.name, "name@0.1.0");
@@ -85,15 +70,14 @@ Deno.test("release() creates initial release", async () => {
 });
 
 Deno.test("release() creates update release", async () => {
-  await using git = await tempRepository();
-  const repo = fakeRepository({ git });
-  await git.commits.create("previous", { allowEmpty: true });
-  await git.tags.create("name@1.2.3");
-  await git.commits.create("feat: new feature", { allowEmpty: true });
-  const pkg = await testPackage(git.path(), {
-    name: "@scope/name",
-    version: "1.3.0",
+  await using pkg = await tempPackage({
+    config: { name: "@scope/name", version: "1.3.0" },
+    commits: [
+      { summary: "initial", tags: ["name@1.2.3"] },
+      { summary: "feat: new feature" },
+    ],
   });
+  const repo = fakeRepository({ git: git({ cwd: pkg.directory }) });
   const [rls, assets] = await release(pkg, { repo });
   assertEquals(rls.tag, "name@1.3.0");
   assertEquals(rls.name, "name@1.3.0");
@@ -115,48 +99,43 @@ Deno.test("release() creates update release", async () => {
 });
 
 Deno.test("release() creates draft release", async () => {
-  await using git = await tempRepository();
-  const repo = fakeRepository({ git });
-  await git.commits.create("previous", { allowEmpty: true });
-  await git.tags.create("name@1.2.3");
-  await git.commits.create("feat: new feature", { allowEmpty: true });
-  const pkg = await testPackage(git.path(), {
-    name: "@scope/name",
-    version: "1.3.0",
+  await using pkg = await tempPackage({
+    config: { name: "@scope/name", version: "1.3.0" },
+    commits: [
+      { summary: "initial", tags: ["name@1.2.3"] },
+      { summary: "feat: new feature" },
+    ],
   });
+  const repo = fakeRepository({ git: git({ cwd: pkg.directory }) });
   const [rls] = await release(pkg, { repo, draft: true });
   assertEquals(rls.draft, true);
 });
 
 Deno.test("release() creates pre-release", async () => {
-  await using git = await tempRepository();
-  const repo = fakeRepository({ git });
-  await git.commits.create("previous", { allowEmpty: true });
-  await git.tags.create("name@1.2.3");
-  const commit = await git.commits.create("feat: new feature", {
-    allowEmpty: true,
+  await using pkg = await tempPackage({
+    config: { name: "@scope/name", version: `1.3.0-pre.1+fedcba9` },
+    commits: [
+      { summary: "initial", tags: ["name@1.2.3"] },
+      { summary: "feat: new feature" },
+    ],
   });
-  const pkg = await testPackage(git.path(), {
-    name: "@scope/name",
-    version: `1.3.0-pre.1+${commit.short}`,
-  });
+  const repo = fakeRepository({ git: git({ cwd: pkg.directory }) });
   const [rls] = await release(pkg, { repo, draft: true });
-  assertEquals(rls.name, `name@1.3.0-pre.1+${commit.short}`);
+  assertEquals(rls.name, `name@1.3.0-pre.1+fedcba9`);
   assertEquals(rls.prerelease, true);
 });
 
 Deno.test("release() updates existing release", async () => {
-  await using git = await tempRepository();
-  const repo = fakeRepository({ git });
+  await using pkg = await tempPackage({
+    config: { name: "@scope/name", version: `1.3.0` },
+    commits: [
+      { summary: "initial", tags: ["name@1.2.3"] },
+      { summary: "feat: new feature" },
+    ],
+  });
+  const repo = fakeRepository({ git: git({ cwd: pkg.directory }) });
   const existing = fakeRelease({ repo, id: 42, tag: "name@1.2.3" });
   repo.releases.list = async () => await Promise.resolve([existing]);
-  await git.commits.create("previous", { allowEmpty: true });
-  await git.tags.create("name@1.2.3");
-  await git.commits.create("feat: new feature", { allowEmpty: true });
-  const pkg = await testPackage(git.path(), {
-    name: "@scope/name",
-    version: "1.3.0",
-  });
   const [rls] = await release(pkg, { repo });
   assertEquals(rls.id, 42);
   assertEquals(
--- tool/forge/testing.ts
+++ tool/forge/testing.ts
@@ -3,47 +3,51 @@
  *
  * ```ts
  * import { tempPackage } from "@roka/forge/testing";
- * await using pkg = await tempPackage({ name: "@scope/name" });
+ * await using pkg = await tempPackage({
+ *   config: { name: "@scope/name" },
+ * });
+ * ```
+ *
+ * ```ts
+ * import { tempWorkspace } from "@roka/forge/testing";
+ * await using packages = await tempWorkspace({
+ *   configs: [
+ *     { name: "@scope/name1" },
+ *     { name: "@scope/name2" },
+ *   ],
+ * });
  * ```
  *
  * @module testing
  */
 
-import { type Config, type Package, packageInfo } from "@roka/forge/package";
+import {
+  type Config,
+  type Package,
+  packageInfo,
+  workspace,
+} from "@roka/forge/package";
+import { tempRepository, type TempRepositoryOptions } from "@roka/git/testing";
 import { join } from "@std/path";
 
-/**
- * Creates a test package under given directory with given configuration.
- *
- * @example
- *
- * ```ts
- * import { testPackage } from "@roka/forge/testing";
- * import { tempDirectory } from "@roka/testing/temp";
- * import { assertEquals } from "@std/assert";
- *
- * await using directory = await tempDirectory();
- * const pkg = await testPackage(directory.path(), {
- *   name: "@scope/name",
- *   version: "1.2.3",
- * });
- *
- * assertEquals(pkg.name, "name");
- * assertEquals(pkg.version, "1.2.3");
- * ```
- */
-export async function testPackage(
-  directory: string,
-  config: Config & { root?: string },
-): Promise<Package> {
-  await Deno.mkdir(directory, { recursive: true });
-  await Deno.writeTextFile(
-    join(directory, "deno.json"),
-    JSON.stringify(config, undefined, 2),
-  );
-  const pkg = await packageInfo({ directory });
-  if (config.root) pkg.root = config.root;
-  return pkg;
+/** Options for the {@linkcode tempPackage} function. */
+export interface TempPackageOptions {
+  /** File contents for package configuration (`deno.json`). */
+  config?: Config;
+  /** Options for the initialization of the git repository. */
+  repo?: TempRepositoryOptions;
+  /** Commits and tags to create in the repository. */
+  commits?: { summary: string; tags?: string[] }[];
+}
+
+/** Options for the {@linkcode tempWorkspace} function. */
+export interface TempWorkspaceOptions {
+  /** File contents for package configurations (`deno.json`). */
+  configs?: Config[];
+  /** Options for the initialization of the git repository. */
+  repo?: TempRepositoryOptions;
+  /** Commits and tags to create in the repository. */
+  commits?: { summary: string; tags?: string[] }[];
 }
 
 /**
@@ -52,27 +56,137 @@ export async function testPackage(
  * The package will be created under a temporary directory, which will be
  * automatically removed when the package is disposed.
  *
- * @example
+ * @example Create a temporary package from configuration.
  *
  * ```ts
  * import { tempPackage } from "@roka/forge/testing";
  * import { assertEquals } from "@std/assert";
  *
  * await using pkg = await tempPackage({
- *   name: "@scope/name",
- *   version: "1.2.3",
+ *   config: { name: "@scope/name", version: "1.2.3" },
  * });
  *
  * assertEquals(pkg.name, "name");
  * assertEquals(pkg.version, "1.2.3");
  * ```
+ *
+ * @example Create a package with given commits and tags.
+ *
+ * ```ts
+ * import { tempPackage } from "@roka/forge/testing";
+ * import { assertEquals } from "@std/assert";
+ *
+ * await using pkg = await tempPackage({
+ *   config: { name: "@scope/name" },
+ *   commits: [
+ *     { summary: "release", tags: ["name@1.2.3"] },
+ *   ],
+ * });
+ *
+ * assertEquals(pkg.version, "1.2.3");
+ * ```
  */
 export async function tempPackage(
-  config: Config,
+  options?: TempPackageOptions,
 ): Promise<AsyncDisposable & Package> {
-  const directory = await Deno.makeTempDir();
-  const pkg = await testPackage(directory, config);
+  const repo = await createRepository(options);
+  await createPackage(repo.path(), options?.config);
+  const pkg = await packageInfo({ directory: repo.path() });
   return Object.assign(pkg, {
-    [Symbol.asyncDispose]: () => Deno.remove(directory, { recursive: true }),
+    [Symbol.asyncDispose]: repo[Symbol.asyncDispose],
   });
+}
+
+/**
+ * Creates a test workspace of packages using given configurations.
+ *
+ * The workspace will be created under a temporary directory, which will be
+ * automatically removed when the workspace is disposed.
+ *
+ * @example Create a temporary workspace from configurations.
+ *
+ * ```ts
+ * import { tempWorkspace } from "@roka/forge/testing";
+ * import { assertEquals } from "@std/assert";
+ *
+ * await using workspace = await tempWorkspace({
+ *   configs: [
+ *     { name: "@scope/name1", version: "1.2.3" },
+ *     { name: "@scope/name2", version: "3.2.1" },
+ *   ],
+ * });
+ * const [pkg1, pkg2] = workspace;
+ *
+ * assertEquals(pkg1?.name, "name1");
+ * assertEquals(pkg2?.name, "name2");
+ * assertEquals(pkg1?.version, "1.2.3");
+ * assertEquals(pkg2?.version, "3.2.1");
+ * ```
+ *
+ * @example Create a workspace with given commits and tags.
+ *
+ * ```ts
+ * import { tempWorkspace } from "@roka/forge/testing";
+ * import { assertEquals } from "@std/assert";
+ *
+ * await using workspace = await tempWorkspace({
+ *   configs: [
+ *     { name: "@scope/name1" },
+ *     { name: "@scope/name2" },
+ *   ],
+ *   commits: [
+ *     { summary: "fix(name1): bug", tags: ["name1@1.2.3"] },
+ *     { summary: "fix(name2): bug", tags: ["name2@3.2.1"] },
+ *   ],
+ * });
+ * const [pkg1, pkg2] = workspace;
+ *
+ * assertEquals(pkg1?.version, "1.2.3");
+ * assertEquals(pkg2?.version, "3.2.1");
+ * ```
+ */
+export async function tempWorkspace(
+  options?: TempWorkspaceOptions,
+): Promise<AsyncDisposable & Package[]> {
+  const repo = await createRepository(options);
+  await Promise.all(
+    (options?.configs ?? []).map((config, index) =>
+      createPackage(
+        repo.path(config.name ?? `package${index}`),
+        config,
+      )
+    ),
+  );
+  await createPackage(repo.path(), {
+    workspace: (options?.configs ?? [])?.map((config, index) =>
+      config.name ?? `package${index}`
+    ),
+  });
+  const packages = await workspace({ directory: repo.path() });
+  return Object.assign(packages, {
+    [Symbol.asyncDispose]: repo[Symbol.asyncDispose],
+  });
+}
+
+async function createRepository(
+  options: TempPackageOptions | TempWorkspaceOptions | undefined,
+) {
+  const repo = await tempRepository(options?.repo);
+  for (const { summary, tags } of options?.commits ?? []) {
+    // deno-lint-ignore no-await-in-loop
+    await repo.commits.create(summary, { allowEmpty: true });
+    for (const tag of tags ?? []) {
+      // deno-lint-ignore no-await-in-loop
+      await repo.tags.create(tag);
+    }
+  }
+  return repo;
+}
+
+async function createPackage(directory: string, config: Config | undefined) {
+  await Deno.mkdir(directory, { recursive: true });
+  await Deno.writeTextFile(
+    join(directory, "deno.json"),
+    JSON.stringify(config ?? {}, undefined, 2),
+  );
 }
