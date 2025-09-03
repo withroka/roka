a2ae5f854384895077a8c7fc7f14a3af5365d3b3
Tugrul Ates <me@tugrulates.com>
2025-09-03 11:48:01 +0200

    feat(http/json/client): implement `RequestOptions` for JSON client (#240)

--- core/http/json/client.test.ts
+++ core/http/json/client.test.ts
@@ -9,8 +9,10 @@ Deno.test("client() can make requests", async (t) => {
   using _fetch = mockFetch(t);
   const api = client("https://api.github.com", { token });
   await t.step("post", async (t) => {
-    issue = await api
-      .post("/repos/withroka/test/issues", { title: "Test issue" });
+    issue = await api.post(
+      "/repos/withroka/test/issues",
+      { body: { title: "Test issue" } },
+    );
     await assertSnapshot(t, issue);
   });
   await t.step("get", async (t) => {
@@ -19,22 +21,23 @@ Deno.test("client() can make requests", async (t) => {
     await assertSnapshot(t, issue);
   });
   await t.step("put", async (t) => {
-    const result = await api
-      .put(`/repos/withroka/test/issues/${issue?.number}/lock`, {
-        lock_reason: "resolved",
-      });
+    const result = await api.put(
+      `/repos/withroka/test/issues/${issue?.number}/lock`,
+      { body: { lock_reason: "resolved" } },
+    );
     await assertSnapshot(t, result);
   });
   await t.step("delete", async (t) => {
-    const result = await api
-      .delete(`/repos/withroka/test/issues/${issue?.number}/lock`);
+    const result = await api.delete(
+      `/repos/withroka/test/issues/${issue?.number}/lock`,
+    );
     await assertSnapshot(t, result);
   });
   await t.step("patch", async (t) => {
-    issue = await api
-      .patch(`/repos/withroka/test/issues/${issue?.number}`, {
-        state: "closed",
-      });
+    issue = await api.patch(
+      `/repos/withroka/test/issues/${issue?.number}`,
+      { body: { state: "closed" } },
+    );
     await assertSnapshot(t, issue);
   });
 });
--- core/http/json/client.ts
+++ core/http/json/client.ts
@@ -8,9 +8,10 @@
  * async function usage() {
  *   interface Issue { number: number; state: string; };
  *   const api = client("https://api.github.com");
- *   await api.post<Issue>("/repos/owner/repo/issues", {
- *     title: "Test issue",
- *   });
+ *   await api.post<Issue>(
+ *     "/repos/owner/repo/issues",
+ *     { body: { title: "Test issue" } },
+ *   );
  *   const issue = await api.get<Issue>("/repos/owner/repo/issues/1");
  * }
  * ```
@@ -18,31 +19,47 @@
  * @module client
  */
 
-import { request } from "../request.ts";
+import { request, type RequestOptions } from "../request.ts";
 
 /** A JSON client returned by the {@linkcode client} function. */
 export interface Client {
   /** Makes a GET request. */
-  get<T>(path: string): Promise<Partial<T>>;
+  get<T>(
+    path: string,
+    options?: JsonRequestOptions,
+  ): Promise<Partial<T>>;
   /** Makes a DELETE request. */
-  delete<T>(path: string): Promise<Partial<T>>;
+  delete<T>(
+    path: string,
+    options?: JsonRequestOptions,
+  ): Promise<Partial<T>>;
   /** Makes a PATCH request. */
-  patch<T>(path: string, body: object): Promise<Partial<T>>;
+  patch<T>(
+    path: string,
+    options?: JsonRequestOptions & { body?: object },
+  ): Promise<Partial<T>>;
   /** Makes a POST request. */
-  post<T>(path: string, body: object): Promise<Partial<T>>;
+  post<T>(
+    path: string,
+    options?: JsonRequestOptions & { body?: object },
+  ): Promise<Partial<T>>;
   /** Makes a PUT request. */
-  put<T>(path: string, body: object): Promise<Partial<T>>;
+  put<T>(
+    path: string,
+    options?: JsonRequestOptions & { body?: object },
+  ): Promise<Partial<T>>;
+  /**
+   * Makes a GET request with optional JSON body and returns the response as a
+   * string.
+   */
+  text(
+    path: string,
+    options?: JsonRequestOptions & { body?: object },
+  ): Promise<string>;
 }
 
-/** Options for the {@linkcode client} function. */
-export interface ClientOptions {
-  /** The bearer token to be sent with the request headers. */
-  token?: string;
-  /** The user agent to be sent with the request headers. */
-  agent?: string;
-  /** The referrer to be sent with the request headers. */
-  referrer?: string;
-}
+/** Options for JSON requests. */
+export type JsonRequestOptions = Omit<RequestOptions, "method" | "body">;
 
 /**
  * Creates an HTTP client for making JSON-based requests.
@@ -65,37 +82,45 @@ export interface ClientOptions {
  *   const api = client("https://api.github.com", {
  *     token: "TOKEN",
  *   });
- *   await api.post<Issue>("/repos/owner/repo/issues", {
- *     title: "Test issue",
- *   });
+ *   await api.post<Issue>(
+ *     "/repos/owner/repo/issues",
+ *     { body: { title: "Test issue" } },
+ *   );
  * }
  * ```
  */
-export function client(url: string, options?: ClientOptions): Client {
+export function client(
+  url: string | URL,
+  options?: JsonRequestOptions,
+): Client {
+  const clientOptions = options;
+  if (typeof url === "string") url = new URL(url);
   async function req<T>(
     method: string,
     path: string,
-    body?: object,
+    options: JsonRequestOptions & { body?: object; text?: boolean } = {},
   ): Promise<Partial<T>> {
-    const response = await request(`${url}${path}`, {
+    const { body, text, ...requestOptions } = options ?? {};
+    const response = await request(new URL(path, url), {
       method,
       headers: {
         "Accept": "application/json; charset=UTF-8",
         ...body && { "Content-Type": "application/json; charset=UTF-8" },
-        ...(options?.token && { "Authorization": `Bearer ${options.token}` }),
-        ...(options?.agent && { "User-Agent": options.agent }),
-        ...(options?.referrer && { "Referrer": options.referrer }),
       },
+      ...clientOptions,
+      ...requestOptions,
       ...body && { body: JSON.stringify(body) },
     });
     if (response.headers.get("Content-Type") === null) return {};
-    return await response.json();
+    return text ? await response.text() as T : await response.json();
   }
   return {
-    get: async (path) => await req("GET", path),
-    delete: async (path) => await req("DELETE", path),
-    patch: async (path, body) => await req("PATCH", path, body),
-    put: async (path, body) => await req("PUT", path, body),
-    post: async (path, body) => await req("POST", path, body),
+    get: async (path, options) => await req("GET", path, options),
+    delete: async (path, options) => await req("DELETE", path, options),
+    patch: async (path, options) => await req("PATCH", path, options),
+    put: async (path, options) => await req("PUT", path, options),
+    post: async (path, options) => await req("POST", path, options),
+    text: async (path, options) =>
+      await req<string>("GET", path, { ...options, text: true }),
   };
 }
