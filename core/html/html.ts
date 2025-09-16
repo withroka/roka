0fe6d2fd135af5c60bc735b79e963c2b37edf340
Tugrul Ates <me@tugrulates.com>
2025-09-16 08:02:35 +0200

    refactor(html): move `plain()` to its own module (#265)

--- core/html/html.ts
+++ core/html/html.ts
@@ -1,11 +1,9 @@
 /**
- * This module provides functions for working with HTML content, and it is
- * complementary to the standard {@link https://jsr.io/@std/html | **@std/html**}
- * library.
+ * A library for working with HTML content, complementary to the standard
+ * {@link https://jsr.io/@std/html | **@std/html**} library.
  *
- * This package currently only provides the {@linkcode plain} function, which
- * removes HTML tags and escapes special characters to produce plain text from
- * HTML input.
+ * This package only provides the {@link [plain]} module to remove HTML tags
+ * from text input.
  *
  * ```ts
  * import { plain } from "@roka/html";
@@ -14,25 +12,3 @@
  *
  * @module html
  */
-
-import { DOMParser } from "@b-fuze/deno-dom";
-
-/**
- * Converts HTML content to plain text by removing tags and decoding special
- * characters.
- *
- * @example Basic usage.
- * ```ts
- * import { plain } from "@roka/html";
- * import { assertEquals } from "@std/assert";
- * const text = plain("<p>Hello, <b>world</b>!</p>");
- * assertEquals(text, "Hello, world!");
- * ```
- */
-export function plain(html: string): string {
-  const doc = new DOMParser().parseFromString(html, "text/html");
-  return doc.body.textContent
-    .replace(/[\u200E-\u200F]/g, "")
-    .replace(/\s+/g, " ")
-    .trim();
-}
---
+++ core/html/plain.ts
@@ -0,0 +1,33 @@
+/**
+ * This module provides the {@linkcode plain} function, which  removes HTML
+ * tags and escapes special characters to produce plain text from HTML input.
+ *
+ * ```ts
+ * import { plain } from "@roka/html";
+ * const text = plain("<p>Hello, <b>world</b>!</p>");  // "Hello, world!"
+ * ```
+ *
+ * @module plain
+ */
+
+import { DOMParser } from "@b-fuze/deno-dom";
+
+/**
+ * Converts HTML content to plain text by removing tags and decoding special
+ * characters.
+ *
+ * @example Basic usage.
+ * ```ts
+ * import { plain } from "@roka/html";
+ * import { assertEquals } from "@std/assert";
+ * const text = plain("<p>Hello, <b>world</b>!</p>");
+ * assertEquals(text, "Hello, world!");
+ * ```
+ */
+export function plain(html: string): string {
+  const doc = new DOMParser().parseFromString(html, "text/html");
+  return doc?.body?.textContent
+    .replace(/[\u200E-\u200F]/g, "")
+    .replace(/\s+/g, " ")
+    .trim();
+}
