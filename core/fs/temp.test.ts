c7a9eba52d9479540c8b1868e965e61712707ab0
Tugrul Ates <me@tugrulates.com>
2025-09-20 12:24:16 +0200

    compare symlink resolved path

--- core/fs/temp.test.ts
+++ core/fs/temp.test.ts
@@ -1,4 +1,5 @@
 import { assertEquals, assertRejects } from "@std/assert";
+import { realPath } from "@std/fs/unstable-real-path";
 import { tempDirectory } from "./temp.ts";
 
 Deno.test("tempDirectory() creates a disposable directory", async () => {
@@ -19,7 +20,7 @@ Deno.test("tempDirectory({ chdir: true }) changes working directory", async () =
   const cwd = Deno.cwd();
   {
     await using directory = await tempDirectory({ chdir: true });
-    assertEquals(Deno.cwd(), directory.path());
+    assertEquals(await realPath(Deno.cwd()), await realPath(directory.path()));
     await Deno.writeTextFile("test.txt", "Hello, world!");
     assertEquals(
       await Deno.readTextFile(directory.path("test.txt")),
