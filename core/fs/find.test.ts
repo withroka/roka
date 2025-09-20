7e86e9071bdd9268c6d0f308389aca1dd228df59
Tugrul Ates <me@tugrulates.com>
2025-09-20 14:30:46 +0200

    iterate

--- core/fs/find.test.ts
+++ core/fs/find.test.ts
@@ -1,87 +1,119 @@
 import { pool } from "@roka/async/pool";
-import { find, type FindOptions } from "@roka/fs/find";
+import { find } from "@roka/fs/find";
 import { type TempDirectory, tempDirectory } from "@roka/fs/temp";
-import { assertEquals } from "@std/assert";
+import { assertEquals, assertRejects } from "@std/assert";
 import { distinct } from "@std/collections/distinct";
-import { dirname, normalize } from "@std/path";
-
-async function test(
-  paths: string[],
-  options?: FindOptions,
-) {
-  await using dir = await tempDirectory();
-  await pool(["a", "b/c"], (x) => Deno.mkdir(dir.path(x), { recursive: true }));
-  await pool(
-    ["a/a.txt", "b/c/c.txt", "d.txt"],
-    (x) => Deno.writeTextFile(dir.path(x), "content"),
-  );
-  const nameFilters = Array.isArray(options?.name)
-    ? options?.name
-    : (typeof options?.name === "string" ? [options.name] : []);
-  const pathFilters = Array.isArray(options?.path)
-    ? options?.path
-    : (typeof options?.path === "string" ? [options.path] : []);
-  const args = [...paths];
-  if (options?.type?.file) args.push("-type", "f");
-  if (options?.type?.directory) args.push("-type", "d");
-  if (nameFilters[0]) args.push("-name", nameFilters[0]);
-  for (const name of nameFilters.slice(1)) args.push("-o", "-name", name);
-  if (pathFilters[0]) args.push("-path", pathFilters[0]);
-  for (const path of pathFilters.slice(1)) args.push("-o", "-path", path);
-  console.log("args:", args);
-  const cmd = new Deno.Command("find", {
-    args,
-    stdout: "piped",
-    stderr: "piped",
-    cwd: dir.path(),
-  });
-  const { stdout, stderr, code } = await cmd.output();
-  if (code !== 0) console.log("stderr:", new TextDecoder().decode(stderr));
-  assertEquals(code, 0);
-  const cwd = Deno.cwd();
-  try {
-    Deno.chdir(dir.path());
-    let posix = new TextDecoder().decode(stdout).split("\n").filter((x) => x);
-    const result = await Array.fromAsync(find(paths, options));
-    console.log("posix:", posix);
-    console.log("result:", result);
-    assertEquals(posix.map(normalize), result);
-  } finally {
-    Deno.chdir(cwd);
-  }
-}
+import { dirname } from "@std/path";
 
 async function createFiles(dir: TempDirectory, files: string[]) {
-  const paths = files.map((x) => dir.path(x));
-  const directories = distinct(paths.map(dirname));
-  await pool(directories, (x) => Deno.mkdir(x, { recursive: true }));
-  await pool(paths, (x) => Deno.writeTextFile(x, "content"));
-  return paths;
+  const directories = distinct(files.map(dirname)).filter((x) => x !== ".");
+  await pool(directories, (x) => Deno.mkdir(dir.path(x), { recursive: true }));
+  await Deno.writeTextFile(dir.path("a.txt"), "content");
+  await pool(files, (x) => Deno.writeTextFile(dir.path(x), "content"));
 }
 
-Deno.test("find() empty", async () => {
-  await using dir = await tempDirectory();
-  createFiles(dir, ["a.txt", "b/c.txt", "d/e/f.txt"]);
-  const cwd = Deno.cwd();
-  try {
-    Deno.chdir(dir.path());
-    const result = await Array.fromAsync(find([], {}));
-    assertEquals(result, []);
-  } finally {
-    Deno.chdir(cwd);
-  }
-  assertEquals(result, []);
+Deno.test("find() return empty results for empty input", async () => {
+  await using dir = await tempDirectory({ chdir: true });
+  await createFiles(dir, ["a.txt", "b/c.txt", "d/e/f.txt"]);
+  assertEquals(await Array.fromAsync(find([])), []);
 });
 
-Deno.test("find() root", () => test(["."], {}));
-Deno.test("find() dir", () => test(["a"], {}));
-Deno.test("find() subdir", () => test(["b/c"], {}));
-Deno.test("find() file", () => test(["d.txt"], {}));
-Deno.test("find() mix", () => test(["a", "b/c", "d.txt"], {}));
-Deno.test("find() type file", () => test(["."], { type: { file: true } }));
-Deno.test("find() type dir", () => test(["."], { type: { directory: true } }));
+Deno.test("find() can find existing file", async () => {
+  await using dir = await tempDirectory({ chdir: true });
+  await createFiles(dir, ["a.txt"]);
+  assertEquals(await Array.fromAsync(find(["a.txt"])), [
+    "a.txt",
+  ]);
+});
 
-Deno.test("find() name", () => test(["."], { name: "c" }));
-Deno.test("find() names", () => test(["."], { name: ["c", "*.txt"] }));
-Deno.test("find() path", () => test(["."], { path: "*/*.txt" }));
-Deno.test("find() paths", () => test(["."], { path: ["?"] }));
+Deno.test("find() skips non-existing file", async () => {
+  await using dir = await tempDirectory({ chdir: true });
+  await createFiles(dir, ["a.txt"]);
+  assertEquals(await Array.fromAsync(find(["b.txt"])), []);
+});
+
+Deno.test("find() can reject non-existing file", async () => {
+  await using dir = await tempDirectory({ chdir: true });
+  await createFiles(dir, ["a.txt"]);
+  assertRejects(
+    () => Array.fromAsync(find(["b.txt"], { validate: true })),
+    Deno.errors.NotFound,
+  );
+});
+
+Deno.test("find() can find multiple files", async () => {
+  await using dir = await tempDirectory({ chdir: true });
+  await createFiles(dir, ["a.txt", "b.txt", "c.txt"]);
+  assertEquals(await Array.fromAsync(find(["b.txt", "c.txt", "d.txt"])), [
+    "b.txt",
+    "c.txt",
+  ]);
+});
+
+Deno.test("find() can find existing directory", async () => {
+  await using dir = await tempDirectory({ chdir: true });
+  await createFiles(dir, ["dir/a.txt"]);
+  assertEquals(await Array.fromAsync(find(["dir"])), [
+    "dir",
+    "dir/a.txt",
+  ]);
+});
+
+Deno.test("find() skips non-existing directory", async () => {
+  await using dir = await tempDirectory({ chdir: true });
+  await createFiles(dir, ["a.txt"]);
+  assertEquals(await Array.fromAsync(find(["dir"])), []);
+});
+
+Deno.test("find() can reject non-existing directory", async () => {
+  await using dir = await tempDirectory({ chdir: true });
+  await createFiles(dir, ["a.txt"]);
+  assertRejects(
+    () => Array.fromAsync(find(["dir"], { validate: true })),
+    Deno.errors.NotFound,
+  );
+});
+
+Deno.test("find() can find multiple directories", async () => {
+  await using dir = await tempDirectory({ chdir: true });
+  await createFiles(dir, ["a.txt", "dir1/b.txt", "dir2/c.txt"]);
+  assertEquals(await Array.fromAsync(find(["dir1", "dir2", "dir3"])), [
+    "dir1",
+    "dir1/b.txt",
+    "dir2",
+    "dir2/c.txt",
+  ]);
+});
+
+Deno.test("find() rejects with AggregateError for multiple failures", async () => {
+  await using dir = await tempDirectory({ chdir: true });
+  await createFiles(dir, ["a.txt"]);
+  assertRejects(
+    () => Array.fromAsync(find(["b.txt", "c.txt"], { validate: true })),
+    AggregateError,
+  );
+});
+
+Deno.test("find() can find files and directories", async () => {
+  await using dir = await tempDirectory({ chdir: true });
+  await createFiles(dir, ["a.txt", "dir1/b.txt", "dir2/c.txt"]);
+  assertEquals(await Array.fromAsync(find(["a.txt", "dir1"])), [
+    "a.txt",
+    "dir1",
+    "dir1/b.txt",
+  ]);
+});
+
+Deno.test("find() can find in subdirectories", async () => {
+  await using dir = await tempDirectory({ chdir: true });
+  await createFiles(dir, ["a.txt", "b/c.txt", "d/e/f.txt"]);
+  assertEquals(await Array.fromAsync(find(["."])), [
+    ".",
+    "a.txt",
+    "b",
+    "b/c.txt",
+    "d",
+    "d/e",
+    "d/e/f.txt",
+  ]);
+});
--- core/fs/find.ts
+++ core/fs/find.ts
@@ -1,23 +1,10 @@
 import { pool } from "@roka/async/pool";
+import { maybe } from "@roka/maybe";
 import { omit } from "@std/collections";
 import { walk } from "@std/fs";
-import { basename, dirname, globToRegExp } from "@std/path";
-
-/**
- * An error thrown by the `input` module.
- */
-export class FindError extends Error {
-  /** Construct InputError. */
-  constructor(
-    message: string,
-    options?: { cause?: unknown },
-  ) {
-    super(message, options);
-    this.name = "InputError";
-  }
-}
 
 export interface FindOptions {
+  validate?: boolean;
   maxDepth?: number;
   type?: {
     file?: boolean;
@@ -30,80 +17,32 @@ export interface FindOptions {
   skip?: RegExp[];
 }
 
-export interface FindOptionsOld {
-  /**
-   * Include files in results.
-   * @default {false}
-   */
-  files?: boolean | string;
-  /**
-   * Include directories in results.
-   * @default {false}
-   */
-  directories?: boolean | string;
-}
-
 export async function* find(
   paths: string[],
   options?: FindOptions,
-): AsyncGenerator<string> {
+): AsyncIterableIterator<string> {
   const walkOptions = options && {
     ...omit(options, ["type"]),
     ...options?.type && { includeFiles: options.type.file === true },
     ...options?.type && { includeDirs: options.type.directory === true },
     ...options?.type && { includeSymlinks: options.type.symlink === true },
   };
-  console.log("walk:", walkOptions);
-  const stats = await pool(paths, async (path) => ({
-    path,
-    stat: await Deno.stat(path),
-  }));
+  const { value: stats, error, errors } = await maybe(() =>
+    pool(paths, async (path) => {
+      const { value: stat, error } = await maybe(() => Deno.stat(path));
+      if (error && options?.validate) throw error;
+      return { path, stat };
+    })
+  );
+  if (error) throw errors.length === 1 ? errors[0] : error;
   for (const { path, stat } of stats) {
+    if (!stat) continue;
     if (stat.isFile) {
       yield path;
     }
     if (stat.isDirectory) {
       for await (const entry of walk(path, walkOptions)) {
         yield entry.path;
-      }
-    }
-  }
-}
-
-export async function* findOld(
-  paths: string[],
-  options?: FindOptionsOld,
-): AsyncGenerator<string> {
-  const { files = false, directories = false } = options ?? {};
-  function check(path: string, check: boolean | string) {
-    return check === true ||
-      (typeof check === "string" && globToRegExp(check).test(basename(path)));
-  }
-  const stats = await pool(paths, async (path) => ({
-    path,
-    stat: await Deno.stat(path),
-  }));
-  for (const { path, stat } of stats) {
-    if (stat.isFile && check(path, files)) yield path;
-    if (stat.isDirectory) {
-      const matched = new Set();
-      if (directories === true) {
-        matched.add(path);
-        yield path;
-      }
-      for await (
-        const entry of walk(path, {
-          includeFiles: (files !== false) || (typeof directories === "string"),
-        })
-      ) {
-        if (entry.isFile) {
-          if (check(entry.path, files)) yield entry.path;
-          const dir = dirname(entry.path);
-          if (!matched.has(dir) && check(entry.path, directories)) {
-            matched.add(dir);
-            yield dir;
-          }
-        }
       }
     }
   }
