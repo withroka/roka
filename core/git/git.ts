1e24b8d7925cb1e2878330dea88bfc866c62f465
Tugrul Ates <me@tugrulates.com>
2025-11-02 13:08:47 +0100

    doc

--- core/git/git.ts
+++ core/git/git.ts
@@ -995,7 +995,7 @@ export interface RemotePushTagOptions
     RemotePushForceOptions,
     RemoteTrackOptions,
     SignOptions {
-  /** Tag to push to remote. */
+  /** Tag or tags to push to remote. */
   tag: string | Tag | (string | Tag)[];
   /**
    * Cannot be specified with {@linkcode RemotePushTagOptions.tag}.
