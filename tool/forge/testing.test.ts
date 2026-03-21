3b1103f10c667ca5cf5fbf95f8f6a38c48dc1faf
Tugrul Ates <me@tugrulates.com>
2026-03-21 22:44:00 +0100

    test(forge): fix tests

--- tool/forge/testing.test.ts
+++ tool/forge/testing.test.ts
@@ -18,7 +18,6 @@ Deno.test("tempPackage() creates a disposable package", async () => {
       directory,
       root: directory,
       config: { name: "@scope/name", version: "1.2.3" },
-      changes: [],
     });
     await Deno.stat(directory);
   }
@@ -67,7 +66,6 @@ Deno.test("tempWorkspace() creates a disposable workspace", async () => {
       directory: join(root, "name"),
       root,
       config: { name: "@scope/name", version: "1.2.3" },
-      changes: [],
     }]);
     await Deno.stat(root);
   }
